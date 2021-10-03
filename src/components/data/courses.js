ce7dd853c46dca99576aac10e155641d2d2a79fe
md.asif.8393 <md.asif.8393@gmail.com>
2021-10-03 23:05:38 +0600

    first commit

---
+++ src/components/data/courses.js
@@ -0,0 +1,88 @@
+const courses = [
+    {
+        name: 'C',
+        instructor: 'Alan',
+        rating: 4.6,
+        price: 13.99,
+        key: '1',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/1200px-C_Programming_Language.svg.png'
+    },
+    {
+        name: 'C++',
+        instructor: 'Abdul Goni',
+        rating: 4.3,
+        price: 9.99,
+        key: '2',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png'
+    },
+    {
+        name: 'Java',
+        instructor: 'Salam Murshidi',
+        rating: 44.7,
+        price: 11.99,
+        key: '3',
+        img: 'https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.png'
+    },
+    {
+        name: 'Java',
+        instructor: 'Salam Murshidi',
+        rating: 44.7,
+        price: 11.99,
+        key: '3',
+        img: 'https://1000logos.net/wp-content/uploads/2020/09/Java-Logo.png'
+    },
+    {
+        name: 'Sumit Singh',
+        age: 67,
+        salary: 108158705,
+        company: 'Chewy Inc',
+        country: 'India',
+        key: '5',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/singh.jpg'
+    },
+    {
+        name: 'Jonathan Gray',
+        age: 52,
+        salary: 107630680,
+        company: 'Blackstone Group Inc',
+        country: 'China',
+        key: '6',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/gray.jpg'
+    },
+    {
+        name: 'Sundar Pichai',
+        age: 45,
+        salary: 86162752,
+        company: 'Google Inc',
+        country: 'India',
+        key: '7',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/pichai.jpg'
+    },
+    {
+        name: 'Satya Nadella',
+        age: 47,
+        salary: 77289149,
+        company: 'Microsoft Corp',
+        country: 'UK',
+        key: '8',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/nadella.jpg'
+    },
+    {
+        name: 'Robert Swan',
+        age: 56,
+        salary: 99022847,
+        company: 'Intel Inc',
+        country: 'Belgium',
+        key: '9',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/swan.jpg'
+    },
+    {
+        name: 'Douglas Ingram',
+        age: 45,
+        salary: 77289149,
+        company: 'Sarepta Therapeuticd Inc',
+        country: 'Turkey',
+        key: '10',
+        img: 'https://www.bloomberg.com/graphics/2020-highest-paid-ceos/img/ingram.jpg'
+    },
+]
